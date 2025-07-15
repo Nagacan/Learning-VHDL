eeefa225f8f8bbe187d4e095d39bb24d43833148
Paul Nguyen <paulnguyen@ucsb.edu>
2025-07-15 04:46:27 -0700

    Update and_gate.vhd

--- Introduction to VHDL/and_gate.vhd
+++ Introduction to VHDL/and_gate.vhd
@@ -4,18 +4,18 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-
+ 
 entity example_and is
-  port(
-    input1 : in std_logic;
-    input2 : in std_logic;
-    and_result : out std_logic
-      ); 
-  end example_and;
-
+  port (
+    input_1    : in  std_logic;
+    input_2    : in  std_logic;
+    and_result : out std_logic
+    );
+end example_and;
+ 
 architecture rtl of example_and is
-  signal and_gate : std_logic;
+  signal and_gate : std_logic;
 begin
-  and_gate <= input1 and input2;
-  and_result <= and_gate;
+  and_gate   <= input_1 and input_2;
+  and_result <= and_gate;
 end rtl;

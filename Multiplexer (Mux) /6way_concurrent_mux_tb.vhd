a5f2c77d6ab0aab66f79f17c61da13077b22702d
Paul Nguyen <paulnguyen@ucsb.edu>
2025-08-30 16:19:34 -0700

    Update 6way_concurrent_mux_tb.vhd

--- Multiplexer (Mux) /6way_concurrent_mux_tb.vhd
+++ Multiplexer (Mux) /6way_concurrent_mux_tb.vhd
@@ -1,35 +1,48 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_1164.all;
 
-entity tb_mux6 is
-end tb_mux6;
+-- Testbenches usually have no ports
+entity mux6_tb is
+end mux6_tb;
 
-architecture Behavioral of tb_mux6 is
+architecture behavior of mux6_tb is
+    -- 1. Declare the component (the Unit Under Test, UUT)
+    component mux6
+        port(
+            a1  : in  std_logic_vector(2 downto 0);
+            a2  : in  std_logic_vector(2 downto 0);
+            a3  : in  std_logic_vector(2 downto 0);
+            a4  : in  std_logic_vector(2 downto 0);
+            a5  : in  std_logic_vector(2 downto 0);
+            a6  : in  std_logic_vector(2 downto 0);
+            sel : in  std_logic_vector(2 downto 0);
+            b   : out std_logic_vector(2 downto 0)
+        );
+    end component;
 
-component mux6 is
-    Port ( A : in  STD_LOGIC;
-           B : in  STD_LOGIC;
-           C : out  STD_LOGIC);
-end component;
-
-signal A1,B1,C1 : std_logic := '0';
+    -- 2. Declare signals to connect to the UUT
+    signal a1, a2, a3, a4, a5, a6 : std_logic_vector(2 downto 0);
+    signal sel : std_logic_vector(2 downto 0);
+    signal b   : std_logic_vector(2 downto 0);
 
 begin
+    -- 3. Instantiate the UUT
+    uut: mux6
+        port map (
+            a1  => a1,
+            a2  => a2,
+            a3  => a3,
+            a4  => a4,
+            a5  => a5,
+            a6  => a6,
+            sel => sel,
+            b   => b
+        );
 
---Instantiate the unit under test(the module we want to test)
-ANDGATE : and_gate port map(
-				A => A1,
-				B => B1,
-				C => C1);
+    -- 4. Stimulus process goes here
+    stim_proc : process
+    begin
+        -- <<< you will write this part >>>
+    end process;
 
-Stimulus_process: process
-begin
-	A1 <= '0';	B1 <= '0';   wait for 100 ns;
-	A1 <= '0';	B1 <= '1';   wait for 100 ns;
-	A1 <= '1';	B1 <= '0';   wait for 100 ns;
-	A1 <= '1';	B1 <= '1';   wait for 100 ns;
-	wait;
-
-end process;
-
-end Behavioral;
+end behavior;
